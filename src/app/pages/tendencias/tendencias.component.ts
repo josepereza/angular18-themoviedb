c505f6bdc6f9050b74a21d2e0637f3c06cfcf398
josepereza <josepereza66@gmail.com>
2024-11-23 09:13:34 +0100

    first commit5

--- src/app/pages/tendencias/tendencias.component.ts
+++ src/app/pages/tendencias/tendencias.component.ts
@@ -1,19 +1,25 @@
-import { Component, inject, OnInit, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, Signal, signal, WritableSignal } from '@angular/core';
 import { ThemovieService } from '../../services/themovie.service';
 import { Result } from '../../interfaces/themovie-response';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { PosterPipe } from '../../pipes/poster.pipe';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+
 
 @Component({
   selector: 'app-tendencias',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule,PosterPipe,MatPaginatorModule], 
+  imports: [MatCardModule, MatButtonModule,PosterPipe,MatPaginatorModule,ReactiveFormsModule], 
   templateUrl: './tendencias.component.html',
   styleUrl: './tendencias.component.css'
 })
-export class TendenciasComponent implements OnInit {
+export class TendenciasComponent implements OnInit{
+  
+  search$ = new FormControl('',{nonNullable: true});
+
 
   themovieSevice=inject(ThemovieService)
 
@@ -22,12 +28,35 @@ export class TendenciasComponent implements OnInit {
   currentPage = signal(1);
   totalPages = signal(0);
   totalItems = signal(0);
+  searchConfir=signal(false);
+  search=signal('');
+  filmSearch$=this.search$.valueChanges.pipe( debounceTime(1000),
+  distinctUntilChanged())
+
 ngOnInit(): void {
+  this.filmSearch$.subscribe(data=>{
+    this.searchConfir.set(true)
+    this.search.set(data)
+    if (data.length<1){
+      this.currentPage.set(1)
+      this.loadMovies()
+    }else {
+          this.loadSearch(this.search())
+
+    }
+  })
 this.loadMovies()
 }
 handlePageEvent(event: PageEvent) {
-  this.currentPage.set(event.pageIndex + 1);
-  this.loadMovies();
+  if (this.searchConfir()){
+      this.currentPage.set(event.pageIndex + 1);
+  this.loadSearch(this.search())
+  }else {
+    this.currentPage.set(event.pageIndex + 1);
+    this.loadMovies();
+
+  }
+
 }
 
 loadMovies(){
@@ -36,6 +65,15 @@ loadMovies(){
     this.themovies.set(data.results);
     this.totalPages.set(data.total_pages);
     this.totalItems.set(data.total_results);
-   })
+   })}
+
+   loadSearch(search:string){
+    this.themovieSevice.getSearch(this.currentPage(),search).subscribe(data=>{
+      this.movies=data.results;
+      this.themovies.set(data.results);
+      this.totalPages.set(data.total_pages);
+      this.totalItems.set(data.total_results);
+     })
 }
+
 }
--- src/app/services/themovie.service.ts
+++ src/app/services/themovie.service.ts
@@ -12,10 +12,17 @@ export class ThemovieService {
 
   urlTrending = 'https://api.themoviedb.org/3/trending/movie/day?language=de-DE';
 
-  urlSearch = 'https://api.themoviedb.org/3/search/movie?query=hacker&language=de-DE';
+  urlSearch = 'https://api.themoviedb.org/3/search/movie';
+  urlSearch2 = 'https://api.themoviedb.org/3/search/movie?query=hacker&language=de-DE';
+
 
   getTrending(page: number):Observable<ThemovieResponse>{
    return this.http.get<ThemovieResponse>(`${this.urlTrending}&page=${page}`)
+  }
+
+  getSearch(page: number,search:string):Observable<ThemovieResponse>{
+    return this.http.get<ThemovieResponse>(`${this.urlSearch}?query=${search}&language=de-DE&page=${page}`)
+
   }
 
 }
