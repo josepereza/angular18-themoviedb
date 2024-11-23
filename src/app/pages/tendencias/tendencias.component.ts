dc30adb72bfe94f31d8b3f6467d98ae49e8cbdd4
josepereza <josepereza66@gmail.com>
2024-11-23 18:41:29 +0100

    first commit6

--- src/app/pages/tendencias/tendencias.component.ts
+++ src/app/pages/tendencias/tendencias.component.ts
@@ -1,18 +1,19 @@
-import { Component, inject, OnDestroy, OnInit, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, Signal, signal, WritableSignal} from '@angular/core';
 import { ThemovieService } from '../../services/themovie.service';
 import { Result } from '../../interfaces/themovie-response';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { PosterPipe } from '../../pipes/poster.pipe';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-tendencias',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule,PosterPipe,MatPaginatorModule,ReactiveFormsModule], 
+  imports: [MatCardModule, MatButtonModule,PosterPipe,MatPaginatorModule,ReactiveFormsModule,MatInputModule,MatFormFieldModule,FormsModule,], 
   templateUrl: './tendencias.component.html',
   styleUrl: './tendencias.component.css'
 })
